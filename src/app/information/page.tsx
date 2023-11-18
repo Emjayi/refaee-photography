b76d12cf07ec1913f18eaa8a53f77989fcefc258
Emjayi <mjs32841@gmail.com>
2023-11-18 18:25:06 +0330

    root layout

---
+++ src/app/information/page.tsx
@@ -0,0 +1,7 @@
+export default function Information() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <h1>Bye World</h1>
+        </main>
+    )
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
+import Link from 'next/link'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +17,21 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <main className="flex flex-col items-center px-24 py-4">
+          <nav className=''>
+            <ul className='flex justify-evenly'>
+              <li className='px-2'>
+                <Link href={"/"}>Home</Link >
+              </li>
+              <li className='px-2'>
+                <Link href={"information"}>Information</Link >
+              </li>
+            </ul>
+          </nav>
+        </main>
+        {children}
+      </body>
     </html>
   )
 }
