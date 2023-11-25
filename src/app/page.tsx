17d70752790683a55354fb16e47792ee353a8237
Emjayi <mjs32841@gmail.com>
2023-11-25 13:01:20 +0330

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <main className="flex flex-col items-center justify-between p-24">
-      <div className='flex min-h-'>
+      <div className='flex'>
         <Image src={"/next.svg"} alt="Sample" width={400} height={560} />
       </div>
     </main>
