15a00c04e831017291af6c9120dbb78fb531d808
Emjayi <mjs32841@gmail.com>
2024-02-05 19:32:08 +0330

    stupid error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import Link from 'next/link'
-import Navbar from './nav'
+import Navbar from './Navbar'
 
 const inter = Inter({ subsets: ['latin'] })
 
