9140d5a7ad4c28d437ea50283256cf874ce35c32
Emjayi <mjs32841@gmail.com>
2024-02-05 20:41:26 +0330

    footer

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
 
           <footer className="footer absolute bottom-0">
             <div className="w-full mx-auto max-w-screen-xl p-4 md:flex md:items-center md:justify-between">
-              <span className="text-sm text-gray-400 sm:text-center">© 2023 <a href="https://refaee.com/" className="hover:underline">Refaee™</a>. All Rights Reserved.
+              <span className="text-sm text-gray-400 sm:text-center">© 2023 <a href="/" className="hover:underline">Refaee™</a>. All Rights Reserved.
               </span>
             </div>
           </footer>
