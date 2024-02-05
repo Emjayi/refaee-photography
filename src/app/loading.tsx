4a0df33557a98ad9645f10f8e1036ae2150c2821
Emjayi <mjs32841@gmail.com>
2024-02-05 20:38:01 +0330

    images loading

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const loading = () => {
     return (
-        <div className='flex h-screen text-center items-center justify-center text-6xl'>
-            <h1>refaee is loading...</h1>
+        <div className='flex h-screen text-center items-center justify-center text-4xl'>
+            <h1 className=' uppercase'>loading...</h1>
         </div>
     )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,37 +4,18 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import Image from 'next/image'
 // import required modules
 import { Autoplay, Keyboard, Navigation, Pagination } from 'swiper/modules';
-import slide1 from "public/bg/bg (1).jpg"
-import slide2 from "public/bg/bg (2).jpg"
-import slide3 from "public/bg/bg (3).jpg"
-import slide4 from "public/bg/bg (4).jpg"
-import slide5 from "public/bg/bg (5).jpg"
 import 'swiper/css';
 import 'swiper/css/navigation';
 
-const images = [
-  {
-    src: slide1,
-    key: 1
-  },
-  {
-    src: slide2,
-    key: 2
-  },
-  {
-    src: slide3,
-    key: 3
-  },
-  {
-    src: slide4,
-    key: 4
-  },
-  {
-    src: slide5,
-    key: 5
-  },
-]
-export default function Home() {
+export default async function Home() {
+
+  const images = await Promise.all([
+    import("public/bg/bg (1).jpg"),
+    import("public/bg/bg (2).jpg"),
+    import("public/bg/bg (3).jpg"),
+    import("public/bg/bg (4).jpg"),
+    import("public/bg/bg (5).jpg")
+  ])
 
   return (
     <div className='flex justify-center items-center '>
@@ -54,9 +35,9 @@ export default function Home() {
         navigation={true}
         modules={[Autoplay, Keyboard, Pagination, Navigation]} className=" w-screen h-screen">
         {
-          images.map((image) => (
-            <SwiperSlide key={image.key}>
-              <Image src={image.src} alt={`Image ${image.key}`} className=' min-h-screen object-cover'></Image>
+          images.map((image, index) => (
+            <SwiperSlide key={index + 1}>
+              <Image src={image.default} alt={`Image ${index + 1}`} className=' min-h-screen object-cover md:!object-fill'></Image>
             </SwiperSlide>
           ))
         }
