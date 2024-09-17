4fc9eb981c10a9af99822f619df359a15b576497
Emjayi <mjs32841@gmail.com>
2024-09-17 17:53:34 +0330

    about page fixed

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -9,16 +9,16 @@ const Page: NextPage = () => {
         <>
             <div className='md:flex items-center bg-black'>
                 <Image alt='About Alireza' src={alireza} className='h-[100dvh] object-contain object-left justify-self-start hidden md:block' />
-                <Image alt='About Alireza' src={alirezaMobile} className='h-[80dvh] object-contain justify-self-start block md:hidden' />
-                <p className='px-8 hidden text-justify py-8 bg-black text-zinc-400'>
-                    Alireza Refaei is a professional photographer and a photography tutorial with over 20 years experience in photography. He is commissioned by leading Saman company. his work on photography can cover products, liquids, landscape, control of lights, life still, architecture and industrial photography.
-                    He has workshops and offline courses for amature and intermediate photographers
-                    pure joy of photography courses has been published for the first time in Iran.
+                <Image alt='About Alireza' src={alirezaMobile} className='h-[80vh] object-contain justify-self-start block md:hidden' />
+                <p className='px-8 block md:hidden text-justify py-8 bg-black text-zinc-400'>
+                    With over 20 years of experience, Alireza Refaei is a professional photographer and photography tutor renowned for his expertise. his work encompasses a diverse range of photographic disciplines, including product photography, liquids, landscapes, advanced lighting control, still life, architecture, and industrial photography.
+
+                    He conducts workshops and offers offline courses tailored for amateur and intermediate photographers. Notably, his "Pure Joy of Photography" courses have been published for the first time in Iran.
                 </p>
-                <p className='px-8 absolute w-[600px] right-32 text-justify py-8 bg-black text-zinc-400'>
-                    Alireza Refaei is a professional photographer and a photography tutorial with over 20 years experience in photography. He is commissioned by leading Saman company. his work on photography can cover products, liquids, landscape, control of lights, life still, architecture and industrial photography.
-                    He has workshops and offline courses for amature and intermediate photographers
-                    pure joy of photography courses has been published for the first time in Iran.
+                <p className='px-8 hidden md:block absolute w-[600px] right-32 text-justify py-8 bg-black text-zinc-400'>
+                    With over 20 years of experience, Alireza Refaei is a professional photographer and photography tutor renowned for his expertise. his work encompasses a diverse range of photographic disciplines, including product photography, liquids, landscapes, advanced lighting control, still life, architecture, and industrial photography.
+
+                    He conducts workshops and offers offline courses tailored for amateur and intermediate photographers. Notably, his "Pure Joy of Photography" courses have been published for the first time in Iran.
                 </p>
             </div>
         </>
